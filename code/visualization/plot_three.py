bd0da855aa04bf41c41412003f05dcc21564f065
manuhuth <s6mahuth@uni-bonn.de>
2022-01-03 13:01:59 +0100

    Update plots.

---
+++ code/visualization/plot_three.py
@@ -0,0 +1,182 @@
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+from functions.plot_tools import get_spline
+from functions.plot_tools import plot_best_strategy
+from functions.plot_tools import stacked_bar
+from functions.plot_tools import plot_bars_vac, compute_deceased, compute_splines_from_results
+
+from numpy import trapz
+
+def autolabel(rects, rounding = 0):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        if rounding == 0:
+            number = '{}'.format(int(np.round(height)))
+        else:
+            number = '{}'.format(np.round(height,rounding))
+            if np.round(height,rounding) == int(np.round(height,rounding)):
+                number = '{}'.format(int(np.round(height,rounding)))
+        ax.annotate(number,
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+appearence = list(range(1, 12))
+vac = "Unequal" #only 
+initial_states = "Unequal"  
+specification = f"inital{initial_states}_vac{vac}"
+
+results = []
+for index in appearence:
+    specification = f"appears_week_{index}"
+    path = (
+        f"/home/manuel/Documents/VaccinationDistribution/code/objects/{specification}.pkl"
+    )
+    
+    with open(
+          path,
+          "rb",
+    ) as input:
+          loaded_object = pickle.load(input)
+    results.append(loaded_object)
+    
+    
+deaths_countryA = pd.DataFrame(data=None, index=range(len(appearence)), columns=["optimal", "pareto", "population"])
+deaths_countryB = deaths_countryA.copy()
+for index in range(len(appearence)):
+    r = results[index]
+    index_optimal = np.argmin(r["all_strategies"]["fval"])
+    index_pareto = np.argmin(r["pareto_improvements"]["fval"])
+    
+    
+    A = [r["all_strategies"].iloc[index_optimal]["countryA"],
+         r["pareto_improvements"].iloc[index_pareto]["countryA"],
+         r["population_based"][0]]
+
+    B = [r["all_strategies"].iloc[index_optimal]["countryB"],
+         r["pareto_improvements"].iloc[index_pareto]["countryB"],
+         r["population_based"][1]]
+    deaths_countryA.iloc[index] = A
+    deaths_countryB.iloc[index] = B
+
+total_deaths = deaths_countryA + deaths_countryB
+
+y_position = 1.08
+y_size = 30
+count_plot = 65
+
+
+fig = plt.figure(constrained_layout=True, figsize = (65,17))
+gs = GridSpec(3, 3, figure=fig)
+
+
+
+xlabel = "Week of mutant appearence"
+#------------------------------------------------------------------------------
+ax = fig.add_subplot(gs[0, 2])
+x = np.array(appearence)  # the label locations
+width = 0.33  # the width of the bars
+rects1 = ax.bar(x, total_deaths["population"]/10**4, width, label = "Population based", color = "C2", alpha = 0.7)
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Deaths in 10.000')
+ax.set_title('Deaths by population-size based vaccine allocation')
+ax.set_xticks(x)
+ax.set_xticklabels(appearence)
+ax.legend()
+ax.set_xlabel(xlabel)
+autolabel(rects1, rounding=2)
+
+ax.text(
+        -0.05,
+        y_position,
+        chr(count_plot),
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax.transAxes,
+        weight="bold",
+        size=y_size,
+)
+count_plot += 1
+#-------------------------------------------------------------------------------
+ax = fig.add_subplot(gs[1, 2])
+
+rects1 = ax.bar(x - width/2,(total_deaths["population"]- total_deaths["optimal"])/10**4, width, label='Optimal', color = "steelblue", alpha = 0.7)
+rects2 = ax.bar(x + width/2, (total_deaths["population"]-total_deaths["pareto"])/10**4, width, label='Pareto optimal', color = "darkorange", alpha=0.7)
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Saved lifes in 10.000')
+ax.set_title('Absolute saved lifes comapred to population-size based vaccine allocation')
+ax.set_xticks(x)
+ax.set_xlabel(xlabel)
+
+ax.set_xticklabels(appearence)
+ax.legend()
+autolabel(rects1, rounding=2)
+autolabel(rects2, rounding=2)
+ax.text(
+        -0.05,
+        y_position,
+        chr(count_plot),
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax.transAxes,
+        weight="bold",
+        size=y_size,
+)
+count_plot += 1
+
+
+#------------------------------------------------------------------------------
+ax = fig.add_subplot(gs[2, 2])
+optimal_percentage = -(total_deaths["optimal"] / total_deaths["population"] - 1)*100
+
+pareto_percentage = -(total_deaths["pareto"] / total_deaths["population"] - 1)*100
+
+rects1 = ax.bar(x - width/2, optimal_percentage, width, label='Optimal', color = "steelblue", alpha = 0.7)
+rects2 = ax.bar(x + width/2,pareto_percentage, width, label='Pareto optimal', color = "darkorange", alpha=0.7)
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Relative number of saved lifes in %')
+ax.set_title('Saved lifes relative to population-size based vaccine allocation')
+ax.set_xticks(x)
+ax.set_xticklabels(appearence)
+ax.legend()
+ax.set_ylim(0, 75)
+ax.set_xlabel(xlabel)
+
+autolabel(rects1)
+autolabel(rects2)
+ax.text(
+        -0.05,
+        y_position,
+        chr(count_plot),
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax.transAxes,
+        weight="bold",
+        size=y_size,
+)
+count_plot += 1
+
+
+
+plt.tight_layout()
+
+fig.savefig(
+    "/home/manuel/Documents/VaccinationDistribution/paper/images/plot_three",
+    bbox_inches="tight",
+)
+
+
+
+
--- code/visualization/plot_two.py
+++ code/visualization/plot_two.py
@@ -5,12 +5,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
 
+from matplotlib.gridspec import GridSpec
+import matplotlib.cm as cm
 from functions.plot_tools import get_spline
 from functions.plot_tools import plot_best_strategy
 from functions.plot_tools import stacked_bar
 from functions.plot_tools import plot_bars_vac, compute_deceased, compute_splines_from_results
+from functions.plot_tools import compute_splines_from_results_initials
 
 from numpy import trapz
 
@@ -49,75 +51,88 @@ add_additional = {"integers" : [9,10],
                  "number" : 0.5}
 
 
-fig = plt.figure(constrained_layout=True, figsize = (23,16))
-gs = GridSpec(2, 3, figure=fig)
+fig = plt.figure(constrained_layout=True, figsize = (31, 23))
+gs = GridSpec(3, 3, figure=fig, height_ratios=[1.1,2,2])
 
+data = [[10,9,8,7,6,5,4,3,2,1,0],
+        [0,1,2,3,4,5,6,7,8,9,10],
+        [0,1,2,3,4,5,6,7,8,9,10],
+        [10,9,8,7,6,5,4,3,2,1,0],
+        ]
+
+ax = fig.add_subplot(gs[0, 0:2]) 
+cmap = cm.get_cmap("Greens", 14)
+hex_col = []
+for i in range(cmap.N):
+    rgba = cmap(i)
+    # rgb2hex accepts rgb or rgba
+    hex_col.append(matplotlib.colors.rgb2hex(rgba))
+
+color_array = pd.DataFrame(columns=range(6), index = range(4))
+for index in range(np.array(data).shape[0]):
+    for index_col in range(np.array(data).shape[1]):
+        color_array.loc[index, index_col] = hex_col[np.array(data)[index, index_col]]
+
+row_labels=["Country A\nwild-type", "Country A\nMutant",
+            "Country B\nwild-type", "Country B\nMutant",]
+col_labels = [f"Case {i}" for i in range(11)]
+x_table = ax.table(cellText=data, rowLabels=row_labels, colLabels= col_labels, loc="center", bbox=[0, 0.13, 1, 0.8],
+                   cellLoc = "center", cellColours=np.array(color_array),)
+ax.set_title("Virus distributions")
+ax.axis("off")
+ax.text(
+        -0.05,
+        y_position,
+        chr(count_plot),
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax.transAxes,
+        weight="bold",
+        size=y_size,
+)
+ax.set_ylim([0.2, 0.9])
+count_plot += 1
 
 
 
    
 dec_A = compute_deceased(results, "countryA")
 dec_B = compute_deceased(results, "countryB")
-colors = ["C1", "C0", "black"]
+colors = ["C1", "C0", "C0"]
 labels = ["Pareto optimal", "Optimal", "Population\nbased"]
 linestyles = ["solid", "solid", "dashed"]
 keys = list(dec_B.keys())
 
-ax = fig.add_subplot(gs[0, 0])  
+ax = fig.add_subplot(gs[1, 0])  
 for index in range(len(dec_B.keys())):
     if keys[index] in ["optimal", "pop"]:
+        if keys[index] == "pop":
+            marker = "o"
+        else:
+            marker = "v"
         ax.scatter(initials, (np.array(dec_A[keys[index]]) + np.array(dec_B[keys[index]]))/100000,
                 label = labels[index], color=colors[index],
-                linestyle=linestyles[index], s=40)
+                linestyle=linestyles[index], s=70, marker=marker)
 
 pop = (np.array(dec_A["pop"]) + np.array(dec_B["pop"]))/100000
 optimal = (np.array(dec_A["optimal"]) + np.array(dec_B["optimal"]))/100000 
 pareto = (np.array(dec_A["pareto"]) + np.array(dec_B["pareto"]))/100000 
 for index in range(len(initials)):
-    ax.plot(np.repeat(initials[index],2), [pop[index], optimal[index]], color = "firebrick" )
+    ax.plot(np.repeat(initials[index],2), [pop[index], optimal[index]], color = "C0" )
     diff = np.round(- pop[index] + optimal[index],2)
     ax.annotate(f"{diff}", (initials[index]+0.1, optimal[index] -diff/2 ) )
     
     
-ax.set_title("Total deaths")
-ax.set_xlabel("Initial mutant (wild-type) infected\nat $t=0$ in country A (B)")
-ax.set_ylabel("Deaths in 100,000")
+ax.set_title("Improvement through optimal strategy")
+
+ax.set_ylabel("Total deaths in 100,000")
 ylim = ax.get_ylim()
+ax.set_xlim(-0.5, 5.5)
+col_label_sym = [f"Case {i}\nCase {6+i}" for i in range(5)] + ["Case 5"]
+ax.set_xticklabels([""] + col_label_sym)
 ax.legend()
 
 
-ax = fig.add_subplot(gs[0, 1])
-for index in range(len(dec_B.keys())):
-    if keys[index] in ["pareto", "pop"]:
-        ax.scatter(initials, (np.array(dec_A[keys[index]]) + np.array(dec_B[keys[index]]))/100000,
-                label = labels[index], color=colors[index],
-                linestyle=linestyles[index], s=40)
-for index in range(len(initials)):
-    ax.plot(np.repeat(initials[index],2), [pop[index], pareto[index]], color = "firebrick" )
-    diff = np.round(- pop[index] + pareto[index],2)
-    ax.annotate(f"{diff}", (initials[index]+0.1, pareto[index] -diff/2 ) )
-    
-    
-ax.set_title("Total deaths")
-ax.set_xlabel("Initial mutant (wild-type) infected\nat $t=0$ in country A (B)")
-ax.set_ylabel("Deaths in 100,000")
-ax.set_ylim(ylim)
-ax.legend()
-
-
-
-
-
-
-ax = fig.add_subplot(gs[0, 1])  
-for index in range(len(dec_A.keys())):
-    keys = list(dec_A.keys())
-    ax.scatter(initials, np.array(dec_A[keys[index]])/100000, label = labels[index], color=colors[index],
-            linestyle=linestyles[index])
-ax.set_title("Deaths in country A")
-ax.set_xlabel("Initial mutant (wild-type) infected\nat $t=0$ in country A (B)")
-ax.set_ylabel("Deaths in 100,000")
-ax.legend()
 ax.text(
         -0.05,
         y_position,
@@ -130,16 +145,33 @@ ax.text(
 )
 count_plot += 1
 
-ax = fig.add_subplot(gs[0, 2])  
+
+
+
+ax = fig.add_subplot(gs[2, 0])
 for index in range(len(dec_B.keys())):
-    keys = list(dec_B.keys())
-    ax.scatter(initials, np.array(dec_B[keys[index]])/100000, label = labels[index], color=colors[index],
-            linestyle=linestyles[index])
-ax.set_title("Deaths in country B")
-ax.set_xlabel("Initial mutant (wild-type) infected\nat $t=0$ in country A (B)")
-ax.set_ylabel("Deaths in 100,000")
+    if keys[index] in ["pareto", "pop"]:
+        if keys[index] == "pop":
+            marker = "o"
+        else:
+            marker = "v"
+        ax.scatter(initials, (np.array(dec_A[keys[index]]) + np.array(dec_B[keys[index]]))/100000,
+                label = labels[index], color="C1",
+                linestyle=linestyles[index], s=70, marker=marker)
+for index in range(len(initials)):
+    ax.plot(np.repeat(initials[index],2), [pop[index], pareto[index]], color = "C1" )
+    diff = np.round(- pop[index] + pareto[index],2)
+    ax.annotate(f"{diff}", (initials[index]+0.1, pareto[index] -diff/2 ) )
+    
+    
+ax.set_title("Improvement through Pareto optimal strategy")
+ax.set_ylabel("")
+ax.set_xlim(-0.5, 5.5)
+ax.set_ylabel("Total deaths in 100,000")
+ax.set_xticklabels([""] + col_label_sym )
+ax.set_ylim(ylim)
 
-
+ax.legend()
 
 
 
@@ -150,18 +182,26 @@ paper_value = 30000
 ylim = (0.2, 0.95)
 
 index = 1
-ax = fig.add_subplot(gs[1, 0])
-fractions = compute_splines_from_results(type_opti = "all_strategies",
-                                      vac = "vac1",)
-ax.plot(initials, fractions, label = "Optimal", color = "C0")
-#ax.fill_between(n_vaccs/2, np.repeat(0, len(fractions)), fractions, color = colors[index], alpha=0.3)
-ax.set_ylabel("Fraction assigned\nto country A")
-ax.plot(initials, np.repeat(0.5, len(fractions)), color= "black", linestyle = "dashed", label = "Population\nbased")
-ax.set_xlabel("Vaccines per week")
-ax.axvline(paper_value, 0, 1, color = "firebrick", linestyle = "dotted", label = "Used in the\nmain part")
-ax.set_title("Optimal strategy (Vaccine one)")
-ax.set_ylim(ylim)
+ax = fig.add_subplot(gs[1, 1])
+fractions = compute_splines_from_results_initials(initials=initials, type_opti = "all_strategies",
+                                      vac = "vac1", results=results)
+
+ax.scatter(initials, fractions,
+           label = "Optimal", color="seagreen",
+           s=70, marker="v")
+ax.scatter(initials, np.repeat(0.5, len(initials)),
+           label = "Population\nbased", color="seagreen",
+           s=70, marker="o")
+for index in range(len(initials)):
+    ax.plot(np.repeat(initials[index],2), [0.5, fractions[index]], color = "seagreen" )
+    diff = np.round(-0.5 + fractions[index],2)
+    ax.annotate(f"{diff}", (initials[index]+0.1, 0.5  + diff/2 ) )
+ax.set_title("Optimal - Difference in\nvaccine one allocation")
+ax.set_ylim([0.27, 0.93])
+ax.set_ylabel("Fraction allocated to country A")
 ax.legend()
+cols_labels_small = [f"Case {i}" for i in range(len(initials))]
+ax.set_xticklabels([""] + cols_labels_small )
 ax.text(
         -0.05,
         y_position,
@@ -173,3 +213,80 @@ ax.text(
         size=y_size,
 )
 count_plot += 1
+
+
+ax = fig.add_subplot(gs[1, 2])
+fractions = compute_splines_from_results_initials(initials=initials, type_opti = "pareto_improvements",
+                                      vac = "vac1", results=results)
+
+ax.scatter(initials, fractions,
+           label = "Pareto optimal", color="seagreen",
+           s=70, marker="v")
+ax.scatter(initials, np.repeat(0.5, len(initials)),
+           label = "Population\nbased", color="seagreen",
+           s=70, marker="o")
+for index in range(len(initials)):
+    ax.plot(np.repeat(initials[index],2), [0.5, fractions[index]], color = "seagreen" )
+    diff = np.round(-0.5 + fractions[index],2)
+    ax.annotate(f"{diff}", (initials[index]+0.1, 0.5  + diff/2 ) )
+ax.set_title("Pareto optimal - Difference in\nvaccine one allocation")
+ax.set_ylim([0.27, 0.93])
+ax.legend()
+ax.set_xticklabels([""] + cols_labels_small )
+ax.text(
+        -0.05,
+        y_position,
+        chr(count_plot),
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax.transAxes,
+        weight="bold",
+        size=y_size,
+)
+
+ax = fig.add_subplot(gs[2, 1])
+fractions = compute_splines_from_results_initials(initials=initials, type_opti = "all_strategies",
+                                      vac = "vac2", results=results)
+
+ax.scatter(initials, fractions,
+           label = "Optimal", color="seagreen",
+           s=70, marker="v")
+ax.scatter(initials, np.repeat(0.5, len(initials)),
+           label = "Population\nbased", color="seagreen",
+           s=70, marker="o")
+for index in range(len(initials)):
+    ax.plot(np.repeat(initials[index],2), [0.5, fractions[index]], color = "seagreen" )
+    diff = np.round(-0.5 + fractions[index],2)
+    ax.annotate(f"{diff}", (initials[index]+0.1, 0.5  + diff/2 ) )
+ax.set_title("Optimal - Difference in\nvaccine two allocation")
+ax.set_ylabel("Fraction allocated to country A")
+ax.set_ylim([0.27, 0.93])
+ax.set_xticklabels([""] + cols_labels_small )
+ax.legend()
+
+
+ax = fig.add_subplot(gs[2, 2])
+fractions = compute_splines_from_results_initials(initials=initials, type_opti = "pareto_improvements",
+                                      vac = "vac1", results=results)
+
+ax.scatter(initials, fractions,
+           label = "Pareto optimal", color="seagreen",
+           s=70, marker="v")
+ax.scatter(initials, np.repeat(0.5, len(initials)),
+           label = "Population\nbased", color="seagreen",
+           s=70, marker="o")
+for index in range(len(initials)):
+    ax.plot(np.repeat(initials[index],2), [0.5, fractions[index]], color = "seagreen" )
+    diff = np.round(-0.5 + fractions[index],2)
+    ax.annotate(f"{diff}", (initials[index]+0.1, 0.5  + diff/2 ) )
+ax.set_title("Pareto optimal - Difference in\nvaccine two allocation")
+ax.set_ylim([0.27, 0.93])
+ax.legend()
+ax.set_xticklabels([""] + cols_labels_small )
+
+
+
+fig.savefig(
+    "/home/manuel/Documents/VaccinationDistribution/paper/images/plot_two",
+    bbox_inches="tight",
+)
